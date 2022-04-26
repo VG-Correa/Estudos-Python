71e326f455974c92e417370772fa53dfb2bfc0e1
VG-Correa <v.victorgabriel2014@gmail.com>
2022-04-26 11:17:33 -0300

    Adicionado mais códigos na aula6.py

--- Aula6.py
+++ Aula6.py
@@ -1,2 +1,11 @@
 print("Hello World")
 print("Olá mundo")
+
+nome_completo = input("Qual seu nome completo? ")
+
+print(nome_completo)
+
+
+idade = input("Quantos anos você tem? ")
+
+print(f"voce têm {idade} anos de idade!")
