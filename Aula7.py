4b84d27bc32ac7fc187d8d3bcc9e89db92684957
VG-Correa <v.victorgabriel2014@gmail.com>
2022-04-26 13:16:57 -0300

    Adicionado Aula7 sobre funções

--- Aula7.py
+++ Aula7.py
@@ -1,3 +1,24 @@
-minha_string = "Qualquer texto"
+#def BigMac():
+#    print("Sanduiche BigMac")
 
-print(f"concatenar {minha_string} em string")
+def fazerBigMac(nome):
+    print(f"Sanduíche do {nome}")
+
+def fazerBatata(tamanho):
+    print(f"Batata do tamanho {tamanho}")
+
+def prepararRefri(tipo,tamanho):
+    print(f"Preparar Refrigerante {tipo} do tamanho {tamanho}")
+
+def fazerComboBigMac(nome_pedido,tamanho_batata,tipo_refri,tamanho_refri):
+    fazerBigMac(nome_pedido)
+    fazerBatata(tamanho_batata)
+    prepararRefri(tipo_refri,tamanho_refri)
+
+
+Nome_Cleinte = input("Qual o seu nome? ")
+TamanhoBatata = input("Qual o tamanho da batata? ")
+TipoRefri = input("Qual o refrigerante que você deseja? ")
+TamanhoRefri = input("Qual o tamanho do refrigerante? ")
+
+fazerComboBigMac(Nome_Cleinte,TamanhoBatata,TipoRefri,TamanhoRefri)
