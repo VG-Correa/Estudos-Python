fca2336b3395542c6bb9b3cfbff372bd20848759
VG-Correa <v.victorgabriel2014@gmail.com>
2022-04-27 18:48:40 -0300

    Adicionado pasta Módulos para criação de funções juntamente com a aula13

---
+++ MÓDULOS/Aula13.py
@@ -0,0 +1,5 @@
+import toolsAula13
+
+Nome1 = "Victor Gabriel Corrêa"
+
+print(toolsAula13.Primeiro_Nome(Nome1))
---
+++ MÓDULOS/toolsAula13.py
@@ -0,0 +1,4 @@
+
+def Primeiro_Nome(Nome):
+    Nome.index(" ",1)
+    print(Nome[0:Nome.index(" ",1)])
