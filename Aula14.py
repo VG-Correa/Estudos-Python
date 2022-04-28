d74f5ce601361de39943b961b138c82ad1b6f272
VG-Correa <v.victorgabriel2014@gmail.com>
2022-04-28 17:29:21 -0300

    Aula sobre classes e importar classes para usa-las individualmente

---
+++ Aula14.py
@@ -0,0 +1,19 @@
+from CLASSES import Carros
+
+fusca = Carros.Carro("Volks","Fusca","Azul","Gasolina",False,0)
+
+fusca.Ligar()
+fusca.Ligar()
+fusca.Acelerar()
+fusca.Acelerar()
+fusca.Acelerar()
+fusca.Acelerar()
+fusca.Acelerar()
+fusca.Frear()
+fusca.Frear()
+fusca.Frear()
+fusca.Frear()
+fusca.Frear()
+fusca.Frear()
+fusca.Ligar()
+fusca.Desligar()
