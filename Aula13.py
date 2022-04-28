0540fe2662e8b08a1b28228fc17af1a21260e554
VG-Correa <v.victorgabriel2014@gmail.com>
2022-04-28 17:28:24 -0300

    Aula sobre importação de funções através de módulos

---
+++ Aula13.py
@@ -0,0 +1,7 @@
+# import toolsAula13
+from MÓDULOS import toolsAula13 #Importa da pasta que está localizada no diretório principal
+
+Nome1 = "Victor Gabriel Corrêa"
+
+print(toolsAula13.Primeiro_Nome(Nome1))
+
